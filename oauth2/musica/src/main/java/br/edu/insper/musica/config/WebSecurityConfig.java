be1f1b28ed846fa43766974cea8a506fe9cb6d9d
Eduardo Santana <ezambomsantana@gmail.com>
2025-04-14 11:19:45 -0300

    cors

--- oauth2/musica/src/main/java/br/edu/insper/musica/config/WebSecurityConfig.java
+++ oauth2/musica/src/main/java/br/edu/insper/musica/config/WebSecurityConfig.java
@@ -10,17 +10,24 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig {
+public class WebSecurityConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/public/**").permitAll()
-                        .requestMatchers("/role/**").hasAuthority("TESTE")
                         .anyRequest()
                         .authenticated()
                 )
